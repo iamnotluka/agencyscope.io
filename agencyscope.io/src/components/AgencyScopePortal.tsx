0ea188fae383dcaa6be5d0a89bfd7642721fc17d
iamnotluka <zoricluk@gmail.com>
2023-06-09 23:00:43 +1000

    Adding portal and login pages

---
+++ agencyscope.io/src/components/AgencyScopePortal.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AgencyScopePortal = () => {
+  return (
+    <div>
+      <h1>This is Agency Scope Portal</h1>
+    </div>
+  )
+}
+
+export default AgencyScopePortal;
